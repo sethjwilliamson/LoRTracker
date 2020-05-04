2435ce8e818af44d9c2391959d3b576a04b91e35
Seth <sethjwilliamson@gmail.com>
2020-05-03 21:05:18 -0500

    Create graveyard window

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function createWindow () {
 
   mainWindow.loadFile('index.html');
   
-  testWindow = new BrowserWindow({
+  fullCardWindow = new BrowserWindow({
     width:340,
     height:512,
     maximizable:false,
@@ -34,29 +34,45 @@ function createWindow () {
       nodeIntegration:true
     }
   })
-  testWindow.loadFile("./previewCard.html");
+  fullCardWindow.loadFile("./previewCard.html");
+
+  graveyardWindow = new BrowserWindow({
+    width: 172,
+    height: 800,
+    minWidth:172,
+    icon: "./icon.png",
+    maximizable:false,
+    transparent:true,
+    frame:false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration:true
+    }
+  })
+
+  graveyardWindow.loadFile('graveyard.html');
   
-  testWindow.webContents.on('did-finish-load', () => {
-    testWindow.setVisibleOnAllWorkspaces(true);
-    testWindow.setAlwaysOnTop(true, 'screen-saver');
-    testWindow.setIgnoreMouseEvents(true);
-    testWindow.hide();
+  fullCardWindow.webContents.on('did-finish-load', () => {
+    fullCardWindow.setVisibleOnAllWorkspaces(true);
+    fullCardWindow.setAlwaysOnTop(true, 'screen-saver');
+    fullCardWindow.setIgnoreMouseEvents(true);
+    fullCardWindow.hide();
   })
 
   ipcMain.on('preview', (event, src, x, y) => {
     mainPos = mainWindow.getPosition();
     if (mainPos[0] > screen.getPrimaryDisplay().workAreaSize.width / 2) { // config
-      testWindow.setPosition(mainPos[0] - 340, mainPos[1] - 210 + y); 
+      fullCardWindow.setPosition(mainPos[0] - 340, mainPos[1] - 210 + y); 
     }
     else {
-      testWindow.setPosition(mainPos[0] + mainWindow.getSize()[0], mainPos[1] - 210 + y); 
+      fullCardWindow.setPosition(mainPos[0] + mainWindow.getSize()[0], mainPos[1] - 210 + y); 
     }
-    testWindow.webContents.send('preview', src, x, y);
-    testWindow.show();
+    fullCardWindow.webContents.send('preview', src, x, y);
+    fullCardWindow.show();
   });
 
   ipcMain.on('unpreview', (event) => {
-    testWindow.hide();
+    fullCardWindow.hide();
   });
 
   mainWindow.webContents.on('did-finish-load', () => {
@@ -121,6 +137,10 @@ global.decklist = [];
 function waitingForGame(r) {
   console.log("Waiting");
 
+  if (!r) {
+    setTimeout(function() {httpGet(url).then(res => waitingForGame(res));}, 500);
+    console.log("r NOT EXIST1");
+  }
   if ((r.GameState) === ('InProgress')) {
     prevDraw = null;
     cardsLeft = 40;
@@ -132,6 +152,11 @@ function waitingForGame(r) {
 }
 
 function matchFound(r) {
+  if (!r) {
+    console.log("r NOT EXIST2");
+    setTimeout(function() {httpGet(url).then(res => waitingForGame(res));}, 500);
+  }
+
   global.decklist = r.CardsInDeck;
 
   mainWindow.show();
@@ -147,6 +172,10 @@ function matchFound(r) {
 }
 
 function waitingForMulligan(r) { //Mulligan  
+  if (!r) {
+    console.log("r NOT EXIST3");
+    setTimeout(function() {httpGet(url).then(res => waitingForGame(res));}, 500);
+  }
   var card = null;
   var firstCard = null;
   
@@ -168,7 +197,6 @@ function waitingForMulligan(r) { //Mulligan
     for (let element of r.Rectangles) {
 
       if ((element.CardCode !== ("face")) && (element.LocalPlayer) && (element.CardID !== firstCard)) {
-        console.log(element.CardCode);
         cardsLeft--;
         
         if (element.type === "Unit") 
@@ -185,6 +213,11 @@ function waitingForMulligan(r) { //Mulligan
 }
 
 function trackingGame(r) {
+  if (!r) {
+    console.log("r NOT EXIST 4")
+    setTimeout(function() {httpGet(url).then(res => waitingForGame(res));}, 500);
+  }
+
   var tempHandSize = 0;
 
   if (r.GameState !== ("InProgress")) {
@@ -220,6 +253,10 @@ function trackingGame(r) {
 }
 
 function matchOver(r) {
+  if (!r)
+    httpGet(url).then(res => waitingForGame(res));
+
+
   if (r.LocalPlayerWon)
     console.log("Victory");
   else
