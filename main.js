642373189be02984cec92eb417ce91d23ccff9c6
Seth <sethjwilliamson@gmail.com>
2020-06-03 15:40:49 -0500

    Update update alerts

--- main.js
+++ main.js
@@ -39,30 +39,24 @@ const data = new Store({
 const { autoUpdater } = require("electron-updater");
 
 autoUpdater.on('checking-for-update', () => {
-  sendStatusToWindow('Checking for update...');
+  sendStatusToWindow('Checking for update...', false);
 });
 autoUpdater.on('update-available', info => {
-  sendStatusToWindow('Update available.');
+  sendStatusToWindow('Update available.', true);
 });
 autoUpdater.on('update-not-available', info => {
-  sendStatusToWindow('Update not available.');
+  sendStatusToWindow('Update not available.', false);
 });
 autoUpdater.on('error', err => {
-  sendStatusToWindow(`Error in auto-updater: ${err.toString()}`);
+  sendStatusToWindow(`Error in auto-updater: ${err.toString()}`, true);
 });
 autoUpdater.on('download-progress', progressObj => {
   sendStatusToWindow(
-    `Download speed: ${progressObj.bytesPerSecond} - Downloaded ${progressObj.percent}% (${progressObj.transferred} + '/' + ${progressObj.total} + )`
+    `Download speed: ${progressObj.bytesPerSecond} - Downloaded ${progressObj.percent}% (${progressObj.transferred} + '/' + ${progressObj.total} + )`, false
   );
 });
 autoUpdater.on('update-downloaded', info => {
-  sendStatusToWindow('Update downloaded; will install now');
-});
-
-autoUpdater.on('update-downloaded', info => {
-  // Wait 5 seconds, then quit and install
-  // In your application, you don't need to wait 500 ms.
-  // You could call autoUpdater.quitAndInstall(); immediately
+  sendStatusToWindow('Update downloaded; will install now', false);
   autoUpdater.quitAndInstall();
 });
 
@@ -396,9 +390,9 @@ app.on('activate', function () {
 })
 
 
-const sendStatusToWindow = (text) => {
+const sendStatusToWindow = (text, alert) => {
   log.info(text);
-  if (mainWindow) {
+  if (mainWindow && alert) {
     mainWindow.webContents.send('message', text);
   }
 };
