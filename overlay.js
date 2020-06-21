d0ca2505544216f9ea633336cdcc40da9ff870a7
Seth <sethjwilliamson@gmail.com>
2020-06-20 22:02:45 -0500

    Revert "Add runetiera functions"
    This reverts commit 5891a13ad217f8aaac9d41843cb8dc41095cd28e.

--- overlay.js
+++ overlay.js
@@ -1,31 +1,28 @@
 const {ipcRenderer, remote} = require('electron');
 
-var width = 1920;
-var height = 1080;
+//const {ratings} = import(
+//    /* webpackIgnore: true */ 'https://runetiera.com/ratings.js'
+//);
+
+
+
 var cOverlay = document.getElementById("overlay");
 var ctxOverlay = cOverlay.getContext("2d");
 var imgSide = new Image;
 imgSide.src = "./images/side-grade.png";
 var imgSideRight = new Image;
 imgSideRight.src = "./images/side-grade-flipped.png";
+console.log(cOverlay.height)
+console.log(cOverlay.width)
+//ctxOverlay.scaleWidth(1)
 ctxOverlay.canvas.width = 1920;
 ctxOverlay.canvas.height = 1080;
 ctxOverlay.textAlign = "center";
 ctxOverlay.fillStyle = "#715726";
 
-ipcRenderer.on("startOverlay", function(event, width2, height2) {
-    width = width2;
-    height = height2;
 
-    cOverlay.width = width2;
-    cOverlay.height = height2;
-
-    ctxOverlay.canvas.width = width2;
-    ctxOverlay.canvas.height = height2;
-})
-
-ipcRenderer.on("expedition", function (event, state, width, height) {
-    drawExpedition(remote.getGlobal('exRectangles'), state);
+ipcRenderer.on("expedition", function (event, state) {
+    drawExpedition(remote.getGlobal('exRectangles'), state)
 })
 
 async function drawExpedition(exRectangles, state) {
@@ -37,22 +34,22 @@ async function drawExpedition(exRectangles, state) {
 
     for (let card of exRectangles) {
         console.log(ratings)
-        if (card.TopLeftX > width * 0.1) {
+        if (card.TopLeftX > 1920 * 0.1) {
             if (state === "Picking") {
                 ctxOverlay.font = "40px BeaufortforLOL-Bold";
-                ctxOverlay.drawImage(imgSide, card.TopLeftX - card.Height * .625, height - card.TopLeftY + card.Height * .125, card.Height * .75, card.Height * .75);
-                ctxOverlay.fillText(ratings[card.CardCode][1], card.TopLeftX - card.Height * .25, height - card.TopLeftY + card.Height * .65)
+                ctxOverlay.drawImage(imgSide, card.TopLeftX - card.Height * .625, 1080 - card.TopLeftY + card.Height * .125, card.Height * .75, card.Height * .75);
+                ctxOverlay.fillText(ratings[card.CardCode][1], card.TopLeftX - card.Height * .25, 1080 - card.TopLeftY + card.Height * .65)
 
             }
             else {
                 ctxOverlay.font = "60px BeaufortforLOL-Bold";
                 if (card.TopLeftX < 1920 * 0.6) {
-                    ctxOverlay.drawImage(imgSide, card.TopLeftX - card.Height, height - card.TopLeftY + card.Height * .125, card.Height * .75, card.Height * .75);
-                    ctxOverlay.fillText(ratings[card.CardCode][1], card.TopLeftX - card.Height * .6, height - card.TopLeftY + card.Height * .6)
+                    ctxOverlay.drawImage(imgSide, card.TopLeftX - card.Height, 1080 - card.TopLeftY + card.Height * .125, card.Height * .75, card.Height * .75);
+                    ctxOverlay.fillText(ratings[card.CardCode][1], card.TopLeftX - card.Height * .6, 1080 - card.TopLeftY + card.Height * .6)
                 }
                 else {
-                    ctxOverlay.drawImage(imgSideRight, card.TopLeftX + card.Width, height - card.TopLeftY + card.Height * .125, card.Height * .75, card.Height * .75);
-                    ctxOverlay.fillText(ratings[card.CardCode][1], card.TopLeftX + card.Width + card.Height * .4, height - card.TopLeftY + card.Height * .6)
+                    ctxOverlay.drawImage(imgSideRight, card.TopLeftX + card.Width, 1080 - card.TopLeftY + card.Height * .125, card.Height * .75, card.Height * .75);
+                    ctxOverlay.fillText(ratings[card.CardCode][1], card.TopLeftX + card.Width + card.Height * .4, 1080 - card.TopLeftY + card.Height * .6)
                 }
             }
         }
