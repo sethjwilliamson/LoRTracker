12e7633bc7911f526ffef75e5470474a710d9efb
Seth <sethjwilliamson@gmail.com>
2020-06-22 00:13:14 -0500

    Update overlay.js

--- overlay.js
+++ overlay.js
@@ -3,7 +3,8 @@ const {ipcRenderer, remote} = require('electron');
 //const {ratings} = import(
 //    /* webpackIgnore: true */ 'https://runetiera.com/ratings.js'
 //);
-
+var width = 1920;
+var height = 1080;
 
 
 var cOverlay = document.getElementById("overlay");
@@ -12,18 +13,26 @@ var imgSide = new Image;
 imgSide.src = "./images/side-grade.png";
 var imgSideRight = new Image;
 imgSideRight.src = "./images/side-grade-flipped.png";
-console.log(cOverlay.height)
-console.log(cOverlay.width)
 //ctxOverlay.scaleWidth(1)
-ctxOverlay.canvas.width = 1920;
-ctxOverlay.canvas.height = 1080;
+ctxOverlay.canvas.width = width;
+ctxOverlay.canvas.height = height;
 ctxOverlay.textAlign = "center";
 ctxOverlay.fillStyle = "#715726";
 
+ipcRenderer.on("startOverlay", function(event, width2, height2) {
+    width = width2;
+    height = height2;
 
-ipcRenderer.on("expedition", function (event, state) {
-    drawExpedition(remote.getGlobal('exRectangles'), state)
+    cOverlay.width = width2;
+    cOverlay.height = height2;
+
+    ctxOverlay.canvas.width = width2;
+    ctxOverlay.canvas.height = height2;
 })
+
+ipcRenderer.on("expedition", function (event, state, width, height) {
+    drawExpedition(remote.getGlobal('exRectangles'), state);
+});
 
 async function drawExpedition(exRectangles, state) {
     const { ratings } = await import(
@@ -31,25 +40,29 @@ async function drawExpedition(exRectangles, state) {
     );
     ctxOverlay.clearRect(0, 0, 10000, 10000);
 
+    
+    ctxOverlay.font = "40px BeaufortforLOL-Bold";
+    ctxOverlay.fillText("Ratings Provided by Runetiera", width * .615, height * 3 / 4)
+
 
     for (let card of exRectangles) {
         console.log(ratings)
-        if (card.TopLeftX > 1920 * 0.1) {
+        if (card.TopLeftX > width * 0.1) {
             if (state === "Picking") {
                 ctxOverlay.font = "40px BeaufortforLOL-Bold";
-                ctxOverlay.drawImage(imgSide, card.TopLeftX - card.Height * .625, 1080 - card.TopLeftY + card.Height * .125, card.Height * .75, card.Height * .75);
-                ctxOverlay.fillText(ratings[card.CardCode][1], card.TopLeftX - card.Height * .25, 1080 - card.TopLeftY + card.Height * .65)
+                ctxOverlay.drawImage(imgSide, card.TopLeftX - card.Height * .625, height - card.TopLeftY + card.Height * .125, card.Height * .75, card.Height * .75);
+                ctxOverlay.fillText(ratings[card.CardCode][1], card.TopLeftX - card.Height * .25, height - card.TopLeftY + card.Height * .65)
 
             }
             else {
                 ctxOverlay.font = "60px BeaufortforLOL-Bold";
-                if (card.TopLeftX < 1920 * 0.6) {
-                    ctxOverlay.drawImage(imgSide, card.TopLeftX - card.Height, 1080 - card.TopLeftY + card.Height * .125, card.Height * .75, card.Height * .75);
-                    ctxOverlay.fillText(ratings[card.CardCode][1], card.TopLeftX - card.Height * .6, 1080 - card.TopLeftY + card.Height * .6)
+                if (card.TopLeftX < width * 0.6) {
+                    ctxOverlay.drawImage(imgSide, card.TopLeftX - card.Height, height - card.TopLeftY + card.Height * .125, card.Height * .75, card.Height * .75);
+                    ctxOverlay.fillText(ratings[card.CardCode][1], card.TopLeftX - card.Height * .6, height - card.TopLeftY + card.Height * .6)
                 }
                 else {
-                    ctxOverlay.drawImage(imgSideRight, card.TopLeftX + card.Width, 1080 - card.TopLeftY + card.Height * .125, card.Height * .75, card.Height * .75);
-                    ctxOverlay.fillText(ratings[card.CardCode][1], card.TopLeftX + card.Width + card.Height * .4, 1080 - card.TopLeftY + card.Height * .6)
+                    ctxOverlay.drawImage(imgSideRight, card.TopLeftX + card.Width, height - card.TopLeftY + card.Height * .125, card.Height * .75, card.Height * .75);
+                    ctxOverlay.fillText(ratings[card.CardCode][1], card.TopLeftX + card.Width + card.Height * .4, height - card.TopLeftY + card.Height * .6)
                 }
             }
         }
