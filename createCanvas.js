913ffbfecc7f9453753eacc2ea6ed877de47e332
Seth <sethjwilliamson@gmail.com>
2020-07-21 15:43:14 -0500

    Update createCanvas.js

--- createCanvas.js
+++ createCanvas.js
@@ -65,8 +65,25 @@ module.exports = {
         }
 
         cCard.style.marginBottom = margin + "px";
-        cCard.addEventListener('mouseenter', function() {previewCard(element.cardCode, this)});
-        cCard.addEventListener('mouseleave', function() {unpreviewCard()})
+        cCard.addEventListener('mouseenter', function() {
+            if (remote.getCurrentWindow().accessibleTitle !== "main") {
+                $("#plusMinus").css({
+                    "top": Math.round(this.getBoundingClientRect()['y']) + "px",
+                    "display": "block"
+                })
+            }
+            console.log(this.getBoundingClientRect()['y'] + "px")
+            console.log(Math.round(this.getBoundingClientRect()['y']) + "px");
+            previewCard(element.cardCode, this)
+        });
+        cCard.addEventListener('mouseleave', function() {
+            if (remote.getCurrentWindow().accessibleTitle !== "main") {
+                $("#plusMinus").css({
+                    "display": "none"
+                })
+            }
+            unpreviewCard()
+        })
         ctxCard = cCard.getContext("2d"); 
         ctxCard.scale(cCard.width / 720, cCard.height / 100);
 
@@ -78,10 +95,17 @@ module.exports = {
         ctxCard.drawImage(element.image,50,0);
         
         grd.addColorStop(0,"black");
+        grd.addColorStop(.5,"black");
         grd.addColorStop(1,"transparent");
         
         ctxCard.fillStyle = grd;
         ctxCard.fillRect(50,0,600,100);
+
+
+        ctxCard.fillStyle = "white";
+        ctxCard.textAlign = "left";
+        ctxCard.font = "55px BeaufortforLOL-Bold";
+        ctxCard.fillText(element.name, 110, 67);
         
         grd2.addColorStop(0,"transparent");
         grd2.addColorStop(1, regionColors[element.region]);
@@ -97,6 +121,7 @@ module.exports = {
         grd3.addColorStop(1, "#289DA1");
         grd3.addColorStop(0, "#1A2F64");
         grd3.addColorStop(.5, "#1A2F64");
+
         
         ctxCard.fillStyle = '#0E4F72';
         roundRect(ctxCard, 0, 0, 100, 100, 15, true,false);
@@ -111,10 +136,6 @@ module.exports = {
         ctxCard.textAlign = "center";
         ctxCard.fillText(element.mana, 50, 76);
         ctxCard.fillText(element.quantity, 670, 76);
-        
-        ctxCard.textAlign = "left";
-        ctxCard.font = "38px BeaufortforLOL-Bold";
-        ctxCard.fillText(element.name, 110, 63);
 
         if (element.quantity == 0) {
             ctxCard.fillStyle ="rgba(50, 50, 50, 0.7)";
@@ -123,25 +144,66 @@ module.exports = {
 
         cCard.addEventListener('click', function(event) {
             console.log(remote.getCurrentWindow().accessibleTitle);
-            if (event.x < $(document).width() / 720 * 620 ^ config.get("swap-increment")) {
-                if (remote.getCurrentWindow().accessibleTitle === "tracker") {
-                    updateCard(element.cardCode, element.type === "Unit", -1)
+            if (event.x < $(document).width() / 720 * 620 && event.x > $(document).width() / 720 * 520) {
+                console.log(event.y)
+                console.log($("#plusMinus").offset().top + $("#plusMinus").height() / 2)
+                if (event.y > $("#plusMinus").offset().top + $("#plusMinus").height() / 2) {
+                    console.log("-")
+                    if (remote.getCurrentWindow().accessibleTitle === "tracker") {
+                        updateCard(element.cardCode, element.type === "Unit", -1)
+                    }
+                    else {
+                        ipcRenderer.send("arrUpdate", remote.getCurrentWindow().accessibleTitle, element.cardCode, -1);
+                    }
                 }
                 else {
-                    ipcRenderer.send("arrUpdate", remote.getCurrentWindow().accessibleTitle, element.cardCode, -1);
-                }
-            }
-            else {
-                if (remote.getCurrentWindow().accessibleTitle === "tracker") {
-                    updateCard(element.cardCode, element.type === "Unit", 1)
-                }
-                else {
-                    ipcRenderer.send("arrUpdate", remote.getCurrentWindow().accessibleTitle, element.cardCode, 1);
+                    console.log("+")
+                    if (remote.getCurrentWindow().accessibleTitle === "tracker") {
+                        updateCard(element.cardCode, element.type === "Unit", +1)
+                    }
+                    else {
+                        ipcRenderer.send("arrUpdate", remote.getCurrentWindow().accessibleTitle, element.cardCode, +1);
+                    }
                 }
             }
         })
  
         div.append($(cCard));
       }
+      
+    cCard = document.createElement("canvas"); 
+    cCard.style.width = "100%";
+    cCard.height = 40;
+    cCard.style.position = "absolute";
+    cCard.style.pointerEvents = "none"
+    cCard.style.display = "none"
+    cCard.id = "plusMinus"
+    console.log(cCard)
+
+    ctxCard = cCard.getContext("2d"); 
+    ctxCard.scale(cCard.width / 720, cCard.height / 100);
+
+    
+    ctxCard.fillStyle = '#404040';
+    //roundRect(ctxCard, 620, 0, 100, 100, 15, true,false);
+    //ctxCard.fillRect(620, 0, 50, 100);
+    
+    roundRect(ctxCard, 520, 0, 100, 100, 15, true,false);
+    ctxCard.fillRect(570, 0, 50, 100);
+
+    ctxCard.fillStyle = '#191919';
+    ctxCard.fillRect(520, 50, 100, 2);
+    ctxCard.fillRect(620, 0, 2, 100);
+
+    
+    ctxCard.font = "60px BeaufortforLOL-Bold";
+    ctxCard.fillStyle = "white";
+    ctxCard.textAlign = "center";
+    ctxCard.fillText("+", 570, 42);
+    
+    ctxCard.font = "80px BeaufortforLOL-Bold";
+    ctxCard.fillText("-", 570, 95);
+
+    div.append($(cCard))
     }
   };
